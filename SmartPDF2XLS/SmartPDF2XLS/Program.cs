80f3a6804f266cd7cbb0f375094bf1047b953764
marting <jmruiz@sciodev.com>
2018-10-12 14:00:14 -0500

    Added the project template

---
+++ SmartPDF2XLS/SmartPDF2XLS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartPDF2XLS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
